e8b15b4033d9e317b0663ac23d596cfd30f13e84
Cherif <32360655+m2cci-CLI@users.noreply.github.com>
2019-02-13 11:45:06 +0100

    Update Method_02_Test.java
    TEST2 FAIT

--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -12,7 +12,76 @@ import static org.junit.Assert.*;
 /**
  * Exercice 02 - Redéfinition
  */
+public class Method_02_Test {package java8.ex02;
+
+import java8.data.Data;
+import java8.data.Person;
+import java8.ex02.Method_02_Test.IDao;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.*;
+
+/**
+ * Exercice 02 - RedÃ©finition
+ */
 public class Method_02_Test {
+
+    // tag::IDao[]
+    interface IDao {
+        List<Person> findAll();
+
+        // TODO crÃ©er une mÃ©thode String format()
+        // TODO la mÃ©thode retourne une chaÃ®ne de la forme [<nb_personnes> persons]
+        // TODO exemple de rÃ©sultat : "[14 persons]", "[30 persons]"
+        
+        public default String format(){
+        	
+        		return "["+this.findAll().size()+ "persons]" ;
+        	}
+        }
+    
+    // end::IDao[]
+
+    // tag::DaoA[]
+    class DaoA implements IDao {
+
+        List<Person> people = Data.buildPersonList(20);
+
+        @Override
+        public List<Person> findAll() {
+            return people;
+        }
+
+        // TODO redÃ©finir la mÃ©thode String format()
+        // TODO la mÃ©thode retourne une chaÃ®ne de la forme DaoA[<nb_personnes> persons]
+        // TODO exemple de rÃ©sultat : "DaoA[14 persons]", "DaoA[30 persons]"
+        // TODO l'implÃ©mentation rÃ©utilise la mÃ©thode format() de l'interface
+       
+        public  String format() {
+        	
+    		return "DaoA["+this.findAll().size()+" "+"persons]" ;
+    	}
+    }
+    
+    // end::DaoA[]
+
+    @Test
+    public void test_daoA_format() throws Exception {
+
+        DaoA daoA = new DaoA();
+
+        // TODO invoquer la mÃ©thode format() pour que le test soit passant
+        String result = daoA.format();
+
+        assertThat(result, is("DaoA[20 persons]"));
+    }
+}
+
 
     // tag::IDao[]
     interface IDao {
