a0f0c1718d9c37ba73b7a039774fa32f0bea73d8
Cherif <32360655+m2cci-CLI@users.noreply.github.com>
2019-02-13 11:44:18 +0100

    Update Method_01_Test.java
    TEST1 FAIT

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -1,12 +1,13 @@
 package java8.ex01;
-
 import java8.data.Data;
 import java8.data.Person;
+import java8.ex01.Method_01_Test.IDao;
+
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -14,55 +15,64 @@ import static org.junit.Assert.*;
  */
 public class Method_01_Test {
 
-    // tag::IDao[]
-    interface IDao {
-        List<Person> findAll();
+	//tag::IDao[]
+	interface IDao {
+		List<Person> findAll();
 
-        // TODO créer une méthode int sumAge()
-        // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
-    }
-    // end::IDao[]
 
-    class DaoA implements IDao {
+		public default  int sumAge(){
+			int somme = 0;
+			for(Person person: findAll()) {
+				somme = somme +person.getAge();
+			}
+			return somme;
+			// TODO Cette méthode retourne le résultat de l'addition des ages des personnes
 
-        List<Person> people = Data.buildPersonList(20);
+		}
 
-        @Override
-        public List<Person> findAll() {
-            return people;
-        }
-    }
+	}
 
-    class DaoB implements IDao {
+// end::IDao[]
 
-        List<Person> people = Data.buildPersonList(100);
+class DaoA implements IDao {
 
-        @Override
-        public List<Person> findAll() {
-            return people;
-        }
-    }
+	List<Person> people = Data.buildPersonList(20);
 
-    @Test
-    public void test_daoA_sumAge() throws Exception {
+	@Override
+	public List<Person> findAll() {
+		return people;
+	}
+}
 
-        DaoA daoA = new DaoA();
+class DaoB implements IDao {
 
-        // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+	List<Person> people = Data.buildPersonList(100);
 
-        assertThat(result, is(210));
-    }
+	@Override
+	public List<Person> findAll() {
+		return people;
+	}
+}
 
-    @Test
-    public void test_daoB_sumAge() throws Exception {
+@Test
+public void test_daoA_sumAge() throws Exception {
 
-        DaoB daoB = new DaoB();
+	DaoA daoA = new DaoA();
 
-        // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+	// TODO invoquer la méthode sumAge pour que le test soit passant
+	int result = daoA.sumAge();
+	assertThat(result, is(210));
+}
 
-        assertThat(result, is(5050));
+@Test
+public void test_daoB_sumAge() throws Exception {
 
-    }
+	DaoB daoB = new DaoB();
+
+	// TODO invoquer la méthode sumAge pour que le test soit passant
+	int result = daoB.sumAge();;
+
+	assertThat(result, is(5050));
+
+}
 }
